b8ec6eeff71c4e854fbe9c8d2a7c20112d1ab8fb
Alexandre Buisson <a.buisson@acropole-expert.com>
2022-04-27 21:56:31 +0200

    Testing Ignite boilerplate

---
+++ ios/Pods/Target Support Files/Pods-PartyHour-PartyHourTests/Pods-PartyHour-PartyHourTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PartyHour_PartyHourTests : NSObject
+@end
+@implementation PodsDummy_Pods_PartyHour_PartyHourTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-PartyHour/Pods-PartyHour-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PartyHour : NSObject
+@end
+@implementation PodsDummy_Pods_PartyHour
+@end
